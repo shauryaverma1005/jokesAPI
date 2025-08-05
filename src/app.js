f065b97f4f3fd20d3a45682d2f481acd92c4cd7d
Shaurya <omshaurya941@gmail.com>
2025-08-05 22:41:44 +0530

    PACKAGE INSTALLED: dotenv, TASK DONE: 1- connected Database successfully, 2- server connected successfully

--- src/app.js
+++ src/app.js
@@ -15,3 +15,4 @@ app.use(express.json({limit: "16kb"}));
 app.use(express.urlencoded({extended : true, limit: "16kb"}));
 app.use(express.static("../public"));
 
+export {app}
--- src/db/index.js
+++ src/db/index.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import { DB_NAME } from "../constants";
+import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
     try {
@@ -11,4 +11,4 @@ const connectDB = async () => {
     }
 }
 
-export { connectDB };
+export { connectDB }; 
